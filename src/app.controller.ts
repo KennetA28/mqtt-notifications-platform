ebc4956bb5bc7e1ed71fa3f9321b9b53c5a7197d
KennetAvila28 <19091097@alumno.utmetropolitana.edu.mx>
2025-08-11 02:30:11 -0600

    add apk upload endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  BadRequestException,
+  Controller,
+  Get,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
 import { AppService } from './app.service';
-
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
@@ -8,5 +16,25 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post('apk')
+  @UseInterceptors(
+    FileInterceptor('file', {
+      storage: diskStorage({
+        destination: './uploads',
+      }),
+      fileFilter: (req, file, cb) => {
+        // Filtra archivos que no sean APK
+        if (!file.originalname.match(/\.(apk)$/)) {
+          return cb(new BadRequestException('Only APK files are allowed!'), false);
+        }
+        cb(null, true);
+      },
+    }),
+  )
+  async uploadAPK(@UploadedFile() file: Express.Multer.File): Promise<any> {
+      return await this.appService.getAPKInfo(file.path);
+    
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,11 +1,21 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { MqttService } from './mqtt/mqtt.service';
-
+import { Apk} from 'node-apk'
 @Injectable()
 export class AppService {
   constructor(private mqttService:MqttService){}
   getHello(): string {
 
     return this.mqttService.publish("/info", "Hello from nest and mqtt");
+  }
+
+ async getAPKInfo(path:string):Promise<any>{
+
+    const apk = new Apk(path)
+    const {versionCode, versionName, package:id} = await apk.getManifestInfo()
+    const certs = await apk.getCertificateInfo()
+    apk.close()
+
+    return {versionCode, versionName, id, certs}
   }
 }
